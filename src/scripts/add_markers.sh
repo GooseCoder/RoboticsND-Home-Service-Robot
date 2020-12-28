1e672d32bade8e284ce960cdbf50b43cdf3e9c66
Gustavo Cruz <gustavo.cruz@processmaker.com>
2020-12-28 10:47:05 -0400

    Fixed the sh script files.

--- src/scripts/add_markers.sh
+++ src/scripts/add_markers.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+export ROBOT_INITIAL_POSE="-x 0 -y -3 -Y 1.5"
 xterm -e "roslaunch my_robot turtlebot_world.launch" &
 sleep 5
 xterm -e "roslaunch my_robot amcl_map.launch" &
 sleep 5
 xterm -e "roslaunch my_robot view_navigation.launch" &
 sleep 5
-xterm -e "rosrun add_markers add_markers"
+xterm -e "rosrun add_markers add_markers_timer"
--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export ROBOT_INITIAL_POSE="-x 0 -y -3 -Y 1.5"
 xterm -e "roslaunch my_robot turtlebot_world.launch" &
 sleep 5
 xterm -e "roslaunch my_robot amcl_map.launch" &
